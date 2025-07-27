630f940d51d6e13ea493eaf0362dff9a58a2d8fc
Akif Kömürcü <akif.komurcu@ext.etiya.com>
2025-07-27 19:30:51 +0300

    Initial Commit

---
+++ TapLocksTests/TapLocksTests.swift
@@ -0,0 +1,17 @@
+//
+//  TapLocksTests.swift
+//  TapLocksTests
+//
+//  Created by KOMURCU on 27.07.2025.
+//
+
+import Testing
+@testable import TapLocks
+
+struct TapLocksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
