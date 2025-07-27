57c5257d7e8dc474630e26f73594b72d9fbd13dc
akifkomurcu <akif.tonmaister58@gmail.com>
2025-07-27 19:30:51 +0300

    Initial Commit

---
+++ TapLocks/TapLocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapLocksApp.swift
+//  TapLocks
+//
+//  Created by KOMURCU on 27.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct TapLocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
